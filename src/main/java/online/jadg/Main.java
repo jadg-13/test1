be568b7214a7931d38011019535890035be1cdab
jadg13 <86695996+jadg-13@users.noreply.github.com>
2024-09-27 19:11:17 -0600

    Add Product Class

--- src/main/java/online/jadg/Main.java
+++ src/main/java/online/jadg/Main.java
@@ -1,7 +1,13 @@
 package online.jadg;
 
+import online.jadg.model.Product;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Product product = new Product(1, "Product 1", 100);
+        System.out.println(product.getId());
+        System.out.println(product.getName());
+        System.out.println(product.getPrice());
     }
 }
